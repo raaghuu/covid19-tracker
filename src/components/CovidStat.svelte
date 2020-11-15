9e54829b864cf5ba6522416cc7833a7e373689c3
Raghav Sharma <sharma.raghav34@gmail.com>
2020-11-16 01:46:53 +0530

    got error pages up and running

--- src/components/CovidStat.svelte
+++ src/components/CovidStat.svelte
@@ -1,5 +1,12 @@
 <script>
-  export let totStats;
+  export let cases;
+  export let deaths;
+  export let recovered;
+  export let updated;
+  export let tested;
+  export let hospitalized;
+  export let ventilators;
+  export let icu;
 </script>
 
 <style>
@@ -11,10 +18,10 @@
 <div class="section">
   <div class="container">
     <div class="columns">
-      <h2 class="is-3 column">Cases: {totStats.cases}</h2>
-      <h2 class="is-3 column">Deaths: {totStats.deaths}</h2>
-      <h2 class="is-3 column">Recovered: {totStats.recovered}</h2>
-      <h2 class="is-3 column">Updated: {totStats.updated}</h2>
+      <h2 class="is-3 column">Cases: {cases}</h2>
+      <h2 class="is-3 column">Deaths: {deaths}</h2>
+      <h2 class="is-3 column">Recovered: {recovered}</h2>
+      <h2 class="is-3 column">Updated: {updated}</h2>
     </div>
   </div>
 </div>
@@ -22,10 +29,10 @@
 <div class="section">
   <div class="container">
     <div class="columns">
-      <h2 class="is-3 column">Tested: {totStats.tested}</h2>
-      <h2 class="is-3 column">Hospitalized: {totStats.hospitalized}</h2>
-      <h2 class="is-3 column">Total in ICU: {totStats.icu}</h2>
-      <h2 class="is-3 column">Total on Ventilator: {totStats.ventilators}</h2>
+      <h2 class="is-3 column">Tested: {tested}</h2>
+      <h2 class="is-3 column">Hospitalized: {hospitalized}</h2>
+      <h2 class="is-3 column">Total in ICU: {icu}</h2>
+      <h2 class="is-3 column">Total on Ventilator: {ventilators}</h2>
     </div>
   </div>
 </div>

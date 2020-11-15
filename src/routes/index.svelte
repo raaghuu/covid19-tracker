508a113d3ce8f46a4aad09b6dd5e97bc1caba0de
Raghav Sharma <sharma.raghav34@gmail.com>
2020-11-15 22:41:27 +0530

    about page and page titles updated

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,15 @@
   import TableContainer from '../components/TableContainer.svelte';
 </script>
 
+<svelte:head>
+  <title>Covid-19 Tracker</title>
+</svelte:head>
+
+<div class="section header">
+  <div class="container">
+    <h1>Covid-19 Tracker</h1>
+  </div>
+</div>
 <CovidStat />
 <CovidChart />
 <TableContainer />
